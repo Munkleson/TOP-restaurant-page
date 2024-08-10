d13bca4fda508a388174c2ca33de508e5b21b85a
Munkleson <alexwongsydney@gmail.com>
2024-08-10 12:39:51 +0900

    Made event listeners for all buttons to change the content of the page depending on what is clicked. Content is all within objects set in pageLoad, though may have to be put elsewhere for organisation in future

--- src/index.js
+++ src/index.js
@@ -1,13 +1,79 @@
-alert("I Love Saori");
-
-
 import './styles.css';
-import { pageLoad } from './pageLoad';
-
-
-
-
-
-
+import { removeAllElements, pageLoad, homeObject, menu, about, contentDiv } from './pageLoad';
 
 pageLoad();
+
+const homeButton = document.querySelector('.homeButton');
+const menuButton = document.querySelector('.menuButton');
+const aboutButton = document.querySelector('.aboutButton');
+
+
+
+homeButton.addEventListener('click', () => {
+    removeAllElements();
+
+    const titleOfMainPage = document.createElement('h1');
+    titleOfMainPage.innerHTML = homeObject.headText;
+
+    const imageDiv = document.createElement('div');
+    const imageIcon = new Image();
+    imageIcon.src = homeObject.image;
+    imageIcon.classList.add('imageIcon');
+    imageDiv.appendChild(imageIcon);
+
+    const mainPageTextDiv = document.createElement('p');
+    mainPageTextDiv.innerHTML = homeObject.bodyText;
+
+    contentDiv.appendChild(titleOfMainPage);
+    contentDiv.appendChild(imageDiv);
+    contentDiv.appendChild(mainPageTextDiv);
+});
+
+menuButton.addEventListener('click', () => {
+    removeAllElements();
+
+    const menuTitle = document.createElement('h1');
+    menuTitle.innerHTML = menu.headText;
+    contentDiv.appendChild(menuTitle);
+
+    for (const element of menu.menuItems) {
+        const itemDiv = document.createElement('div');
+        itemDiv.classList.add('menuItem');
+        contentDiv.appendChild(itemDiv);
+
+        const itemName = document.createElement('p');
+        itemName.innerHTML = "Name: " + element.name;
+        itemDiv.appendChild(itemName);
+
+        const itemPrice = document.createElement('p');
+        itemPrice.innerHTML = "Price: " + element.price;
+        itemDiv.appendChild(itemPrice);
+
+        const itemRating = document.createElement('p');
+        itemRating.innerHTML = "Rating: " + element.rating;
+        itemDiv.appendChild(itemRating);
+    }
+});
+
+aboutButton.addEventListener('click', () => {
+    removeAllElements();
+
+    const headText = document.createElement('h1');
+    headText.innerHTML = about.headText;
+    contentDiv.appendChild(headText);
+
+    const subText = document.createElement('p');
+    subText.innerHTML = about.subText;
+    contentDiv.appendChild(subText);
+
+    const bodyText = document.createElement('p');
+    bodyText.innerHTML = about.bodyText;
+    contentDiv.appendChild(bodyText);
+});
+
+
+
+
+
+
+
--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,22 +1,62 @@
 import purinFood from './food.jpg';
 
-const contentDiv = document.querySelector('#content');
+const homeObject = {
+    headText: "Munkleson's restaurant",
+    image: purinFood,
+    bodyText: "This is my restaurant. I like cooking parakeets, especially rainbow budgees"
+};
+
+const menu = {
+    headText: "Today's menu",
+    menuItems: [
+        {
+            name: "Rainbow budgee",
+            price: "$20",
+            rating: "5/5"
+        },
+        {
+            name: "Rainbow Lorikeet",
+            price: "$40",
+            rating: "10/5"  
+        },
+        {
+            name: "Toucan",
+            price: "$100",
+            rating: "rare/5"
+        }
+    ],
+};
+
+const about = {
+    headText: "About my restaurant",
+    subText: "Lorem ipsum",
+    bodyText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+};
+
+const contentDiv = document.getElementById('content');
+
+const removeAllElements = () => {
+    while(contentDiv.hasChildNodes()){
+        contentDiv.removeChild(contentDiv.firstChild);
+    }
+};
 
 const pageLoad = () => {
     const titleOfMainPage = document.createElement('h1');
-    titleOfMainPage.innerHTML = "Munkleson's Restaurant";
+    titleOfMainPage.innerHTML = homeObject.headText;
 
     const imageDiv = document.createElement('div');
     const imageIcon = new Image();
-    imageIcon.src = purinFood;
+    imageIcon.src = homeObject.image;
     imageIcon.classList.add('imageIcon');
     imageDiv.appendChild(imageIcon);
 
+    const mainPageTextDiv = document.createElement('p');
+    mainPageTextDiv.innerHTML = homeObject.bodyText;
+
     contentDiv.appendChild(titleOfMainPage);
     contentDiv.appendChild(imageDiv);
+    contentDiv.appendChild(mainPageTextDiv);
 }
 
-
-
-
-export { pageLoad };
+export { removeAllElements, pageLoad, homeObject, menu, about, contentDiv };
